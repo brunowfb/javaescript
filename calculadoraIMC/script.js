70edecf5ea857a4afb32e8f58e7408786735effa
Bruno Ferreira Barbosa <bruno_wfb@hotmail.com>
2025-03-25 21:58:25 -0300

    Estudando condições

--- calculadoraIMC/script.js
+++ calculadoraIMC/script.js
@@ -1,20 +1,24 @@
 function calcularIMC() {
-    var altura = (document.getElementById("altura").value) / 100
-    var peso = document.getElementById("peso").value
+    var altura = parseFloat(document.getElementById("altura").value) / 100;
+    var peso = parseFloat(document.getElementById("peso").value);
+    var res = document.getElementById("res");
 
+    var imc = peso / (altura ** 2);
+    
+    let classificacao = '';
 
-    var imc = peso / altura ** 2;
     if (imc < 18.5) {
-        res.innerHTML = "<p>Você está abaixo do peso</p>"
+        classificacao = 'Você está abaixo do peso'
     } else if (imc < 24.9) {
-        res.innerHTML = "<p>Você está com peso norma</p>l"
+        classificacao = 'Você está com peso normal'
     } else if (imc < 29.9) {
-        res.innerHTML = "<p>Você está com sobrepeso</p>"
+        classificacao = 'Você está com sobrepeso'
     } else if (imc < 39.9) {
-        res.innerHTML = "<p>Você está com obesidade</p>"
-    } else if (imc > 39.9) {
-        res.innerHTML = "<p>Você está com obesidade mórbida</p>"
+        classificacao = 'Você está com obesidade'
+    } else {
+        classificacao = 'Você está com obesidade mórbida'
     }
+    res.innerHTML = `<p class="imc-valor">IMC: ${imc.toFixed(2)}</p><p class="classificacap">(${classificacao})</p>`;
 
 }
 
